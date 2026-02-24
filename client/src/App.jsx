84b0af1167edabf98d5d8e8bbee5f31310e15495
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2026-02-24 20:01:25 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
-import Home from './page/Home'
+import React, { useEffect, useState } from 'react'
 import Mainroutes from './routes/Mainroutes'
 
 const App = () => {
+
+  const [isOnline, setIsOnline] = useState(navigator.onLine)
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true)
+    const handleOffline = () => setIsOnline(false)
+
+    window.addEventListener("online", handleOnline)
+    window.addEventListener("offline", handleOffline)
+
+    return () => {
+      window.removeEventListener("online", handleOnline)
+      window.removeEventListener("offline", handleOffline)
+    }
+  }, [])
+
+  if (!isOnline) {
+    return (
+      <div style={{textAlign:"center", marginTop:"100px"}}>
+        <h1>No Internet Connection</h1>
+        <p>Please check your network.</p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Mainroutes />
